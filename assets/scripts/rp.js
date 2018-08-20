0c6d1ae55ca93b9b7917ad9ad72050b897817b7f
Tysheba <tmorgan2dev@gmail.com>
2018-08-20 11:23:55 -0400

    Update reviews to display on screen

--- assets/scripts/rp.js
+++ assets/scripts/rp.js
@@ -1,3 +1,53 @@
+//Capture the star ratings
+var onStar = "";
+var stars = "";
+$(document).ready(function () {
+
+    /* 1. Visualizing things on Hover - See next part for action on click */
+    $('.fa').on('mouseover', function () {
+        var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
+        console.log("mouseover");
+
+        // Now highlight all the stars that's not after the current hovered star
+        $(this).parent().children().each(function (e) {
+            if (e < onStar) {
+                $(this).addClass('hover');
+            }
+            else {
+                $(this).removeClass('hover');
+            }
+        });
+
+    }).on('mouseout', function () {
+        $(this).parent().children().each(function (e) {
+            $(this).removeClass('hover');
+        });
+    });
+
+
+    /* 2. Action to perform on click */
+    $('.fa').on('click', function () {
+        onStar = parseInt($(this).data('value'), 10); // The star currently selected
+        stars = $(this).parent().children();
+
+        for (i = 0; i < stars.length; i++) {
+            $(stars[i]).removeClass('checked');
+        }
+
+        for (i = 0; i < onStar; i++) {
+            $(stars[i]).addClass('checked');
+            console.log(onStar);
+            $("#rated").text("You rated this video " + onStar + " stars");
+        }
+
+
+    });
+
+});
+
+//End capture of stars rating
+
+//Begin functions to get music lyrics, video, and events through Musix Match, YouTube, and Ticketmaster API's
 
 var song = "";
 var artist = '';
@@ -11,134 +61,130 @@ $(document).on("click", "#search", function (event) {
     console.log(song);
     console.log(artist);
     getLyrics();
-    getYTVideo ();
+    getYTVideo();
     $("#eventsdata").empty();
-    showEvents ();
+    showEvents();
+    $(".comment-box").show();
+    $(".savedReviews").hide();
 });
 
-function getLyrics () {
-var queryURL = "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_track="+song+ "&q_artist="+ artist + "&quorum_factor=1&apikey=cad5fbf7afb8c5ce846d140b38ac51ef"
-
-$.ajax({
-    url: queryURL,
-    method: "GET",
-    dataType: "jsonp",
-    jsonp: "callback"
-}).then(function (result) {
-    var trackId = result.message.body.track_list[0].track.track_id;
-    var trackImg = result.message.body.track_list[0].track.album_coverart_100x100
-    console.log(result);
-    console.log(trackId)
-    $("#album-art img").attr("src", trackImg);
-    
-    var queryURL2 = "https://api.musixmatch.com/ws/1.1/track.lyrics.get?format=jsonp&callback=callback&track_id=" + trackId + "&apikey=cad5fbf7afb8c5ce846d140b38ac51ef";
+function getLyrics() {
+    var queryURL = "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_track=" + song + "&q_artist=" + artist + "&quorum_factor=1&apikey=cad5fbf7afb8c5ce846d140b38ac51ef"
 
     $.ajax({
-        url: queryURL2,
+        url: queryURL,
         method: "GET",
         dataType: "jsonp",
-        jsonpCallback: 'jsonp_callback',
-        contentType: 'application/json'
-        // jsonp: "callback"
-    }).then(function (response) {
-        console.log(response);
-        var lyrics = response.message.body.lyrics.lyrics_body
-        // var lyricsBody = response.message.body.lyrics.lyrics_body.split(/\s+/).slice(0,125).join(" ")+ "...";
-        // var commercial = response.message.body.lyrics.lyrics_body.split(/\s+/).slice(125,135).join(" ");
-        if (lyrics == undefined) {
-            $("#showlyrics").html("<h4> No lyrics available for this song</h4> ");
-        }
-        else {
-        $("#showlyrics").html("<h4>" + lyrics + "</h4> ");
-        $("#artist-name").text(artist);
-        }
+        jsonp: "callback"
+    }).then(function (result) {
+        var trackId = result.message.body.track_list[0].track.track_id;
+        var trackImg = result.message.body.track_list[0].track.album_coverart_100x100
+        console.log(result);
+        console.log(trackId)
+        $("#album-art img").attr("src", trackImg);
+
+        var queryURL2 = "https://api.musixmatch.com/ws/1.1/track.lyrics.get?format=jsonp&callback=callback&track_id=" + trackId + "&apikey=cad5fbf7afb8c5ce846d140b38ac51ef";
+
+        $.ajax({
+            url: queryURL2,
+            method: "GET",
+            dataType: "jsonp",
+            jsonpCallback: 'jsonp_callback',
+            contentType: 'application/json'
+            // jsonp: "callback"
+        }).then(function (response) {
+            console.log(response);
+            var lyrics = response.message.body.lyrics.lyrics_body
+
+            if (lyrics == undefined) {
+                $("#showlyrics").html("<h4> No lyrics available for this song</h4> ");
+            }
+            else {
+                $("#showlyrics").html("<h4>" + lyrics + "</h4> ");
+                $("#artist-name").text(artist);
+            }
 
 
+        });
     });
-});
 };
 
-function getYTVideo () {
+// function to get YouTube Video based on Song and Artist selected as keywords
+function getYTVideo() {
 
 
     var ytAPI = "AIzaSyBglfIEbCdN-ZisKCm9RqzCqPKWDIKViAY"
     var input = song + " " + artist
 
 
-console.log (input);
-var ytURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q="+input+"&type=video&key=" + ytAPI
+    console.log(input);
+    var ytURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=" + input + "&type=video&key=" + ytAPI
 
 
-$.ajax({
-    url: ytURL,
-    method: "GET",
+    $.ajax({
+        url: ytURL,
+        method: "GET",
 
-}).then(function (data) {
-    console.log(data);
-    console.log(data.items[0].id.videoId)
+    }).then(function (data) {
+        // console.log(data);
+        console.log(data.items[0].id.videoId)
 
-    var videoId = data.items[0].id.videoId;
-    var videoId2 = data.items[1].id.videoId;
-    var videoId3 = data.items[2].id.videoId;
+        var videoId = data.items[0].id.videoId;
+        var videoId2 = data.items[1].id.videoId;
+        var videoId3 = data.items[2].id.videoId;
 
-    $('.top-video').text("Other Top Videos for " + artist)
-    $(".player").html('<iframe width="560" height="315" src="https://www.youtube.com/embed/' + videoId + '"frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
-    $(".topVideo1").html('<iframe width="560" height="315" src="https://www.youtube.com/embed/' + videoId2 + '"frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
-    $(".topVideo2").html('<iframe width="560" height="315" src="https://www.youtube.com/embed/' + videoId3 + '"frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
-});
+        // Add videos to screen
+        $('.top-video').text("Other Top Videos for " + artist)
+        $(".player").html('<iframe width="560" height="315" src="https://www.youtube.com/embed/' + videoId + '"frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
+        $(".topVideo1").html('<iframe width="560" height="315" src="https://www.youtube.com/embed/' + videoId2 + '"frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
+        $(".topVideo2").html('<iframe width="560" height="315" src="https://www.youtube.com/embed/' + videoId3 + '"frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
+    });
 
 };
 
-// var eventApi = "tD62X5BR9sf5WDs3"
-// var eventURL = "http://api.eventful.com/rest/events/search?...&keywords=books&location=San+Diego&date=Future"
-
-var flickrApi = "47a3a388513c01bbd7dc9cd77581ec75"
-var secret = "caf4cddcd4a12f20"
-var flick  = "https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_m.jpg"
-var flickURL = "https://api.flickr.com/services/feeds/photos_public.gne?tags=kitten&format=json&"
 
 // add function to retrieve upcoming events from ticketmaster API for artist
 
-function showEvents () {
+function showEvents() {
 
 
-var queryURL2 = "https://app.ticketmaster.com/discovery/v1/events.json?keyword="+artist+"&apikey=e24yU8pGFGdyS4VeZAsidGfIG7yZwce1";
+    var queryURL2 = "https://app.ticketmaster.com/discovery/v1/events.json?keyword=" + artist + "&apikey=e24yU8pGFGdyS4VeZAsidGfIG7yZwce1";
 
-$.ajax({
-    url: queryURL2,
-    method: "GET",
-      
-}).then(function (response) {
-    $("#upcoming-events").text("Upcoming Events for artist "+ artist);
-    var ticketMasterEvents = JSON.stringify(response)
-    console.log(response);
-    console.log(response._embedded.events);
-    var events = response._embedded.events;
-    for (var i=0; i < events.length; i++) {
-        // console.log(events[i].name);
-        var eName = events[i].name
-        var eLocation = events[i]._embedded.venue[0].name + " " + events[i]._embedded.venue[0].city.name + ", " + events[i]._embedded.venue[0].state.stateCode;
-        var eDate = events[i].dates.start.dateTime;
-        var eDatef = moment(eDate).format('llll')
-        var eventLink = "https://www.ticketmaster.com/" + events[i]._embedded.attractions[0].url;
-        var $button = $("<button>").html('<a target = "_blank" href='+ eventLink+ '>More Info </a>');
-        // var eInfo = $button.attr("href",eventLink);
-        var eInfo = $button.addClass("btn");
-        console.log(eventLink);
-        var newRow = $("<tr>").append(
-            $("<td>").text(eName),
-            $("<td>").text(eLocation),
-            $("<td>").text(eDatef),
-            $("<td>").html(eInfo)
-          );
-          // Append the new row to the table
-          $("#eventsdata").append(newRow);
-    }
+    $.ajax({
+        url: queryURL2,
+        method: "GET",
 
-}); 
+    }).then(function (response) {
+        $("#upcoming-events").text("Upcoming Events for artist " + artist);
+        var ticketMasterEvents = JSON.stringify(response)
+        // console.log(response);
+        // console.log(response._embedded.events);
+        var events = response._embedded.events;
+        for (var i = 0; i < events.length; i++) {
+            // console.log(events[i].name);
+            var eName = events[i].name
+            var eLocation = events[i]._embedded.venue[0].name + " " + events[i]._embedded.venue[0].city.name + ", " + events[i]._embedded.venue[0].state.stateCode;
+            var eDate = events[i].dates.start.dateTime;
+            var eDatef = moment(eDate).format('llll')
+            var eventLink = "https://www.ticketmaster.com/" + events[i]._embedded.attractions[0].url;
+            var $button = $("<button>").html('<a target = "_blank" href=' + eventLink + '>More Info </a>');
+            // var eInfo = $button.attr("href",eventLink);
+            var eInfo = $button.addClass("btn");
+            // console.log(eventLink);
+            var newRow = $("<tr>").append(
+                $("<td>").text(eName),
+                $("<td>").text(eLocation),
+                $("<td>").text(eDatef),
+                $("<td>").html(eInfo)
+            );
+            // Append the new row to the table
+            $("#eventsdata").append(newRow);
+        }
+
+    });
 
 }
-
+//Clear contact form after submit
 $(document).on("click", "#submit", function (event) {
     event.preventDefault();
     $("#name").val("");
@@ -147,3 +193,93 @@ $(document).on("click", "#submit", function (event) {
     $("#message").val("");
 });
 
+// Collect review data into Firebase
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDrWIRizkRmAuyQ_EDZkd3oQrDzMmJPFdY",
+    authDomain: "rottenpotatoes-213015.firebaseapp.com",
+    databaseURL: "https://rottenpotatoes-213015.firebaseio.com",
+    projectId: "rottenpotatoes-213015",
+    storageBucket: "rottenpotatoes-213015.appspot.com",
+    messagingSenderId: "784045909832"
+};
+firebase.initializeApp(config);
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+};
+
+var database = firebase.database();
+
+// 2. Button for submitting Review
+$(document).on("click", "#submitReview", function (event) {
+    event.preventDefault();
+
+    // Grabs user input and pushes to database
+    var review = $("#review").val().trim();
+
+    // Creates local "temporary" object for holding comments
+    reviewComment = {
+        artist: artist,
+        song: song,
+        rating: onStar,
+        review: review
+    };
+    database.ref().push(reviewComment);
+    console.log(reviewComment);
+
+    //Clear comment box
+    $("#review").val("");
+
+    //Display comments after review is submitted
+    // 3. Create Firebase event for adding a row in the html when a user adds an entry
+    // Since multiple listeners may be attached the firebase "on' event, on page load or reauthentication etc.
+    // We must first remove the existing listener before attaching new listener.
+    // In firebase call off() event before on() event to fix this.
+    database.ref().off();
+    database.ref().on("child_added", function (snap) {
+        console.log(snap.val());
+
+        // Store everything into a variable.
+        var artistName = snap.val().artist;
+        var songName = snap.val().song;
+        var rating = snap.val().rating;
+        var comment = snap.val().review;
+
+        //create paragraph to display comment
+        var $p = $("<p>").addClass("savedReviews");
+        var displayComment = $p.text("Song: " + songName + " by " + artistName + " was rated " + rating + 
+        " stars with the following review comment: " + comment )
+
+        // clear contents and hide comment box then replace with display comment
+        $(".comment-box").hide();
+        $(".comments").append(displayComment);
+
+
+
+        
+
+
+    });
+
+
+});
+
+// Alert submission received with a modal
+        // When the user clicks the button, open the modal
+        // Get the <span> element that closes the modal
+        // var modal = $('#myModal');
+        // var span = $(".close")[0];
+        // modal.style.display = "block";
+
+        // // When the user clicks on <span> (x), close the modal
+        // span.onclick = function () {
+        //     modal.style.display = "none";
+        // }
+
+        // // When the user clicks anywhere outside of the modal, close it
+        // window.onclick = function (event) {
+        //     if (event.target == modal) {
+        //         modal.style.display = "none";
+        //     }
+        // }
